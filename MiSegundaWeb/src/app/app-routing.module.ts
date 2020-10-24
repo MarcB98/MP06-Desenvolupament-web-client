340d30c14b8e4bc8eac3783aa71f0f9f7be9d66e
MarcB98 <marcbatalla652@gmail.com>
2020-10-24 14:11:02 +0200

    MiSegundaWeb 2.0

--- MiSegundaWeb/src/app/app-routing.module.ts
+++ MiSegundaWeb/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { Error404Component } from './error404/error404.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent},
+  { path: 'error404', component: Error404Component},
+  { path: 'eventos', component: EventosComponent},
+  { path: 'about-me', component: AboutMeComponent},
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MiSegundaWeb/src/app/app.module.ts
+++ MiSegundaWeb/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { Error404Component } from './error404/error404.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    EventosComponent,
+    Error404Component,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
